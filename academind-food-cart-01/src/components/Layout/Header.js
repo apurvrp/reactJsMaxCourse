e9f39ce7b96ce6fb7020d2ba3ec4d2dcd9f58903
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-05-04 04:28:21 +0530

    commit :: some changes food code

---
+++ academind-food-cart-01/src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import HeaderCartButton from "./HeaderCartButton";
+import mealsImg from "../../asserts/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImg} alt="A table full of delicious food" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;

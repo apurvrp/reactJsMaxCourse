fd83b27347e4242df78db419f92111dfc6f637b9
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-05-14 02:54:26 +0530

    commit :: some changes in App.js, AddMovie.js and css

--- 14_01_sending_http_requests/src/App1.js
+++ 14_01_sending_http_requests/src/App1.js
@@ -4,7 +4,7 @@ import MoviesList from "./components/MoviesList";
 import "./App.css";
 import AddMovie from "./components/AddMovie";
 
-function App() {
+function App1() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -49,27 +49,25 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch(
-        "https://testing-apis-46c25-default-rtdb.firebaseio.com/movies.json"
-      );
+      const response = await fetch("https://swapi.dev/api/films/");
+      // const response = await fetch(
+      //   "https://testing-apis-46c25-default-rtdb.firebaseio.com/movies.json"
+      // );
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
 
-      const loadedMovies = [];
-
-      for (const key in data) {
-        loadedMovies.push({
-          id: key,
-          title: data[key].title,
-          openingText: data[key].openingText,
-          releaseDate: data[key].releaseDate,
-        });
-      }
-
-      setMovies(loadedMovies);
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -80,20 +78,8 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  async function addMovieHandler(movie) {
-    const response = await fetch(
-      "https://testing-apis-46c25-default-rtdb.firebaseio.com/movies.json",
-      {
-        method: "POST",
-        body: JSON.stringify(movie),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    const data = await response.json();
-    console.log(data);
-    fetchMoviesHandler();
+  function addMovieHandler(movie) {
+    console.log(movie);
   }
 
   let content = <p>Found no movies.</p>;
@@ -123,4 +109,4 @@ function App() {
   );
 }
 
-export default App;
+export default App1;

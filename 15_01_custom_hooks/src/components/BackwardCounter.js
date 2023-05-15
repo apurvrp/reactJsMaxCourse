ae952cc6cf4fb49a8f26ee0dc45fa65968bbb52c
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-05-15 18:41:40 +0530

    commit :: some changes in hooks files

--- 15_01_custom_hooks/src/components/BackwardCounter.js
+++ 15_01_custom_hooks/src/components/BackwardCounter.js
@@ -1,18 +1,10 @@
-import { useState, useEffect } from 'react';
+import React from "react";
 
-import Card from './Card';
+import Card from "./Card";
+import useCounter from "../hooks/useCounter";
 
 const BackwardCounter = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter - 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
+  const counter = useCounter(false);
   return <Card>{counter}</Card>;
 };
 
--- 15_01_custom_hooks/src/components/ForwardCounter.js
+++ 15_01_custom_hooks/src/components/ForwardCounter.js
@@ -1,18 +1,9 @@
-import { useState, useEffect } from 'react';
-
-import Card from './Card';
+import React from "react";
+import Card from "./Card";
+import useCounter from "../hooks/useCounter";
 
 const ForwardCounter = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
+  const counter = useCounter(true);
   return <Card>{counter}</Card>;
 };
 

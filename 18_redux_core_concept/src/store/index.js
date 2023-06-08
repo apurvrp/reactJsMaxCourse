e61342e6c55181c55759b27fbc2b1c5f81a3e420
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-06-08 17:25:27 +0530

    commit :: some changes in counter files

--- 18_redux_core_concept/src/store/index.js
+++ 18_redux_core_concept/src/store/index.js
@@ -1,5 +1,5 @@
-import { createStore } from "redux";
-import { createSlice } from "@reduxjs/toolkit";
+// import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
@@ -14,7 +14,7 @@ const counterSlice = createSlice({
       state.counter--;
     },
     increase(state, action) {
-      state.counter = state.counter + action.amount;
+      state.counter = state.counter + action.payload;
     },
     toggleCounter(state) {
       state.showCounter = !state.showCounter;
@@ -54,6 +54,10 @@ const counterSlice = createSlice({
 //   return state;
 // };
 
-const store = createStore(counterSlice.reducer);
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
+
+export const counterActions = counterSlice.actions;
 
 export default store;

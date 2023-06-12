47027b8ce0f2d767607e0a88e69ce677d1d2b020
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-06-12 19:26:36 +0530

    commit :: some changes in Add cart file

--- 19_advanced_redux/src/App.js
+++ 19_advanced_redux/src/App.js
@@ -2,9 +2,18 @@ import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 function App() {
   const { cartIsVisible: showCart } = useSelector((state) => state.ui);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://testing-apis-46c25-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
--- 19_advanced_redux/src/components/Shop/ProductItem.js
+++ 19_advanced_redux/src/components/Shop/ProductItem.js
@@ -1,12 +1,43 @@
-import { useDispatch } from "react-redux";
+import { useDispatch } from "react-redux"; // useSelector
 import Card from "../UI/Card";
-import classes from "./ProductItem.module.css";
 import { cartActions } from "../../store/cart-slice";
+import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
   const { title, price, description, id } = props;
+  // const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
+
   const addToCartHandler = () => {
+    // const newTotalQuantity = cart.totalQuantity + 1;
+
+    // const updatedItems = cart.items.slice(); // create copy via slice to avoid mutating original state
+    // const existingItem = updatedItems.find((item) => item.id === id);
+    // if (existingItem) {
+    //   const updatedItem = { ...existingItem }; // new object + copy existing properties to avoid state mutation
+    //   updatedItem.quantity++;
+    //   updatedItem.totalPrice = updatedItem.totalPrice + price;
+    //   const existingItemIndex = updatedItems.findIndex(
+    //     (item) => item.id === id
+    //   );
+    //   updatedItems[existingItemIndex] = updatedItem;
+    // } else {
+    //   updatedItems.push({
+    //     id: id,
+    //     price: price,
+    //     quantity: 1,
+    //     totalPrice: price,
+    //     name: title,
+    //   });
+    // }
+
+    // const newCart = {
+    //   totalQuantity: newTotalQuantity,
+    //   items: updatedItems,
+    // };
+
+    // dispatch(cartActions.replaceCart(newCart));
+
     dispatch(
       cartActions.addItemToCart({
         id,

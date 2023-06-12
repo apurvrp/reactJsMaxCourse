0973db72417f4b565cda158971d7b28e54d5af6e
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-06-12 15:16:00 +0530

    commit :: some packages install in repository

--- 19_advanced_redux/src/App.js
+++ 19_advanced_redux/src/App.js
@@ -1,6 +1,6 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 
 function App() {
   return (

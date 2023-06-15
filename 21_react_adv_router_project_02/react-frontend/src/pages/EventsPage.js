9c456814c206a12fba7ac720c2d6c6ca566b753f
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-06-15 18:33:13 +0530

    commit :: some changes in events files

--- 21_react_adv_router_project_02/react-frontend/src/pages/EventsPage.js
+++ 21_react_adv_router_project_02/react-frontend/src/pages/EventsPage.js
@@ -2,7 +2,8 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events;
   return (
     <>
       <EventsList events={events} />
@@ -19,8 +20,9 @@ export const loader = async () => {
   if (!response.ok) {
     // ...
   } else {
-    const resData = await response.json();
-    return resData.events;
+    // const resData = await response.json();
+    // return resData.events;
+    return response;
   }
 };
 

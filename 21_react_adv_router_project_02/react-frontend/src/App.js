2a367c510b15161292f02623a2eee5899db2eecc
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-06-15 17:38:45 +0530

    commit :: some changes in events.js

--- 21_react_adv_router_project_02/react-frontend/src/App.js
+++ 21_react_adv_router_project_02/react-frontend/src/App.js
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./pages/RootLayout";
 import HomePage from "./pages/HomePage";
-import EventsPage from "./pages/EventsPage";
+import EventsPage, { loader as eventsLoader } from "./pages/EventsPage";
 import EventDetailPage from "./pages/EventDetailPage";
 import NewEventPage from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
@@ -21,16 +21,7 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <EventsPage />,
-            loader: async () => {
-              const response = await fetch("http://localhost:8080/events");
-
-              if (!response.ok) {
-                // ...
-              } else {
-                const resData = await response.json();
-                return resData.events;
-              }
-            },
+            loader: eventsLoader,
           },
           { path: ":eventId", element: <EventDetailPage /> },
           { path: "new", element: <NewEventPage /> },
--- 21_react_adv_router_project_02/react-frontend/src/pages/EventsPage.js
+++ 21_react_adv_router_project_02/react-frontend/src/pages/EventsPage.js
@@ -6,8 +6,31 @@ function EventsPage() {
   return (
     <>
       <EventsList events={events} />
+      {/* <EventsList /> */}
     </>
   );
 }
 
 export default EventsPage;
+
+export const loader = async () => {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // ...
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+};
+
+// export async function loader() {
+//   const response = await fetch("http://localhost:8080/events");
+
+//   if (!response.ok) {
+//     // ...
+//   } else {
+//     const resData = await response.json();
+//     return resData.events;
+//   }
+// }

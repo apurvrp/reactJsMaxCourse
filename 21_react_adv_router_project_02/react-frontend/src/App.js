115698143a88987f1bf549a8c69f77127929d90c
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-06-15 17:13:11 +0530

    commit :: some changes in EventPage.js

--- 21_react_adv_router_project_02/react-frontend/src/App.js
+++ 21_react_adv_router_project_02/react-frontend/src/App.js
@@ -1,33 +1,3 @@
-// Challenge / Exercise
-
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// Done
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// Done
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// Done
-// 4. Add properly working links to the MainNavigation
-// Done
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// Done
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// Done
-// 7. Output the ID of the selected event on the EventDetailPage
-// Done
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-// Done
-
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./pages/RootLayout";
 import HomePage from "./pages/HomePage";
@@ -48,7 +18,20 @@ const router = createBrowserRouter([
         path: "events",
         element: <EventRootLayout />,
         children: [
-          { path: "", element: <EventsPage /> },
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: async () => {
+              const response = await fetch("http://localhost:8080/events");
+
+              if (!response.ok) {
+                // ...
+              } else {
+                const resData = await response.json();
+                return resData.events;
+              }
+            },
+          },
           { path: ":eventId", element: <EventDetailPage /> },
           { path: "new", element: <NewEventPage /> },
           { path: ":eventId/edit", element: <EditEventPage /> },
--- 21_react_adv_router_project_02/react-frontend/src/pages/EventsPage.js
+++ 21_react_adv_router_project_02/react-frontend/src/pages/EventsPage.js
@@ -1,35 +1,11 @@
-import { useEffect, useState } from "react";
-
+import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [fetchedEvents, setFetchedEvents] = useState();
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-      const response = await fetch("http://localhost:8080/events");
-
-      if (!response.ok) {
-        setError("Fetching events failed.");
-      } else {
-        const resData = await response.json();
-        setFetchedEvents(resData.events);
-      }
-      setIsLoading(false);
-    }
-
-    fetchEvents();
-  }, []);
+  const events = useLoaderData();
   return (
     <>
-      <div style={{ textAlign: "center" }}>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-      </div>
-      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+      <EventsList events={events} />
     </>
   );
 }

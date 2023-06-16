a3c27ba6e4884b45acf70e921bd1affe9d0f2d9f
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-06-16 20:15:15 +0530

    commit :: some chnages in App.js file

--- 21_react_adv_router_project_02/react-frontend/src/App.js
+++ 21_react_adv_router_project_02/react-frontend/src/App.js
@@ -11,6 +11,7 @@ import EditEventPage from "./pages/EditEventPage";
 import EventRootLayout from "./pages/EventRootLayout";
 import ErrorPage from "./pages/Error";
 import { action as manipulateEventAction } from "./components/EventForm";
+import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 
 const router = createBrowserRouter([
   {
@@ -51,6 +52,11 @@ const router = createBrowserRouter([
             action: manipulateEventAction,
           },
         ],
+      },
+      {
+        path: "newsletter",
+        element: <NewsletterPage />,
+        action: newsletterAction,
       },
     ],
   },

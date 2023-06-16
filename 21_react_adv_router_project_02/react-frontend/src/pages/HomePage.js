b655e07d253a723530cefe87d291d24e05ba8f8a
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-06-17 02:00:54 +0530

    commit :: some changes in Home page

--- 21_react_adv_router_project_02/react-frontend/src/pages/HomePage.js
+++ 21_react_adv_router_project_02/react-frontend/src/pages/HomePage.js
@@ -1,7 +1,12 @@
+import PageContent from "../components/PageContent";
+
 function HomePage() {
   return (
     <>
-      <h1>Home Page</h1>
+      {/* <h1>Home Page</h1> */}
+      <PageContent title="Welcome">
+        <p>Browse all our amazing events</p>
+      </PageContent>
     </>
   );
 }

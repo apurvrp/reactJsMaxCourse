57a5c37b1b3cb8b9c91b108ae8d74dfcd7662e98
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-06-26 23:39:32 +0530

    some changes in meetup file

--- 24_nextjs_course_01/components/meetups/MeetupDetails.js
+++ 24_nextjs_course_01/components/meetups/MeetupDetails.js
@@ -1,12 +1,9 @@
-import classes from './MeetupDetail.module.css';
+import classes from "./MeetupDetails.module.css";
 
 function MeetupDetail(props) {
   return (
     <section className={classes.detail}>
-      <img
-        src={props.image}
-        alt={props.title}
-      />
+      <img src={props.image} alt={props.title} />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>

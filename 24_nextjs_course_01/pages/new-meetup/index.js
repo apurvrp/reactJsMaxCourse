1d491d8fd695c5596da42bd0958035ea01f31daa
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-06-28 00:24:57 +0530

    commit :: some changes in index.js file

--- 24_nextjs_course_01/pages/new-meetup/index.js
+++ 24_nextjs_course_01/pages/new-meetup/index.js
@@ -1,9 +1,23 @@
 // our-domain.com/new-meetup
+import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  function addMeetupHandler(enteredMeetupData) {
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
     console.log(enteredMeetupData);
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+    router.push("/");
   }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;

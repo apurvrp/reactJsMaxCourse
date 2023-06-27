4de3b97ca99827bac341a4f99979a9b8b7a70ed9
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-06-27 17:19:01 +0530

    commit :: some changes in index.js file

--- 24_nextjs_course_01/pages/index.js
+++ 24_nextjs_course_01/pages/index.js
@@ -31,11 +31,23 @@ function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+//   // fetch data from an API
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
 export async function getStaticProps() {
   return {
     props: {
       meetups: DUMMY_MEETUPS,
     },
+    revalidate: 1,
   };
 }
 

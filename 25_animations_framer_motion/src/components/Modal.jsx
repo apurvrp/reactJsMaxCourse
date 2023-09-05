8dac999fd90878ad379392550e72c317acbba15f
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-09-06 01:30:16 +0530

    commit :: some changes in Modal.jsx file

--- 25_animations_framer_motion/src/components/Modal.jsx
+++ 25_animations_framer_motion/src/components/Modal.jsx
@@ -2,13 +2,18 @@ import { createPortal } from "react-dom";
 import { motion } from "framer-motion";
 
 export default function Modal({ title, children, onClose }) {
+  // const hiddenAniamationState = { opacity: 0, y: 70 };
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog
-        initial={{ opacity: 0, y: 70 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 70 }}
+        variants={{
+          hidden: { opacity: 0, y: 70 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
         open
         className="modal"
       >

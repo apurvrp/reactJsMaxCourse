796abcc17aea8fbe5852c5f4068a60c32b35e5e1
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-09-05 23:58:18 +0530

    commit :: some changes in challengeItems.jsx file

--- 25_animations_framer_motion/src/components/ChallengeItem.jsx
+++ 25_animations_framer_motion/src/components/ChallengeItem.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { motion } from "framer-motion";
 
 import { ChallengesContext } from "../store/challenges-context.jsx";
 
@@ -46,7 +47,12 @@ export default function ChallengeItem({
           <p>
             <button onClick={onViewDetails}>
               View Details{" "}
-              <span className="challenge-item-details-icon">&#9650;</span>
+              <motion.span
+                animate={{ rotate: isExpanded ? 180 : 0 }}
+                className="challenge-item-details-icon"
+              >
+                &#9650;
+              </motion.span>
             </button>
           </p>
 

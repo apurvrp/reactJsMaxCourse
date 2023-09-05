80d63d850e77e21d80521bb808f003b6c3481c2b
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-09-06 01:16:25 +0530

    commit :: some changes in Modal.jsx

--- 25_animations_framer_motion/src/components/Header.jsx
+++ 25_animations_framer_motion/src/components/Header.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-
-import NewChallenge from './NewChallenge.jsx';
+import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import NewChallenge from "./NewChallenge.jsx";
 
 export default function Header() {
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
@@ -15,7 +15,9 @@ export default function Header() {
 
   return (
     <>
-      {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      <AnimatePresence>
+        {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      </AnimatePresence>
 
       <header id="main-header">
         <h1>Your Challenges</h1>

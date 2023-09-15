d7f365820b9b58a2c4f744706e6c9cb48bc54fe9
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-09-15 23:29:25 +0530

    commit :: some changes in App.js file

--- 26_01_animations_transition_group/src/App.js
+++ 26_01_animations_transition_group/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from "react-transition-group/Transition";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -8,6 +9,7 @@ import List from "./components/List/List";
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: false,
   };
 
   showModal = () => {
@@ -22,6 +24,37 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={500}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                transition: "opacity 1s ease-out",
+                opacity: state === "exiting" ? 0 : 1,
+              }}
+            />
+          )}
+        </Transition>
+
         <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         <Backdrop show={this.state.modalIsOpen} />
         <button className="Button" onClick={this.showModal}>

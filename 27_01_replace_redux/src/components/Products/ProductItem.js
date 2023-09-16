3c7f25a0b33151d5b0e10e675ec765423e7286e8
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-09-17 01:36:20 +0530

    commit :: some changes in ProductContext file

--- 27_01_replace_redux/src/components/Products/ProductItem.js
+++ 27_01_replace_redux/src/components/Products/ProductItem.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useContext } from "react";
+// import { useDispatch } from 'react-redux';
 
-import Card from '../UI/Card';
-import './ProductItem.css';
-import { toggleFav } from '../../store/actions/products';
+import Card from "../UI/Card";
+import { ProductsContext } from "../../context/products-context";
+import "./ProductItem.css";
+// import { toggleFav } from "../../store/actions/products";
 
-const ProductItem = props => {
-  const dispatch = useDispatch();
-
+const ProductItem = (props) => {
+  // const dispatch = useDispatch();
+  const toggleFav = useContext(ProductsContext).toggleFav;
   const toggleFavHandler = () => {
-    dispatch(toggleFav(props.id));
+    // dispatch(toggleFav(props.id));
+    toggleFav(props.id);
   };
 
   return (
-    <Card style={{ marginBottom: '1rem' }}>
+    <Card style={{ marginBottom: "1rem" }}>
       <div className="product-item">
-        <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
+        <h2 className={props.isFav ? "is-fav" : ""}>{props.title}</h2>
         <p>{props.description}</p>
         <button
-          className={!props.isFav ? 'button-outline' : ''}
+          className={!props.isFav ? "button-outline" : ""}
           onClick={toggleFavHandler}
         >
-          {props.isFav ? 'Un-Favorite' : 'Favorite'}
+          {props.isFav ? "Un-Favorite" : "Favorite"}
         </button>
       </div>
     </Card>
--- 27_01_replace_redux/src/index.js
+++ 27_01_replace_redux/src/index.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { combineReducers, createStore } from "redux";
+import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import productReducer from './store/reducers/products';
+import "./index.css";
+import App from "./App";
+import productReducer from "./store/reducers/products";
+import ProductsProvider from "./context/products-context";
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
+// const rootReducer = combineReducers({
+//   shop: productReducer,
+// });
 
-const store = createStore(rootReducer);
+// const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <Provider store={store}>
+  <ProductsProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </ProductsProvider>,
+  document.getElementById("root")
 );

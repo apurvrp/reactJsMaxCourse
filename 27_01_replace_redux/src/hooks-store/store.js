e1ce0b1fc06c63bc6c116af8e78ab179f720c8d3
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-09-17 23:16:23 +0530

    commit :: some changes in store.js

---
+++ 27_01_replace_redux/src/hooks-store/store.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+let globalState = {};
+let listeners = [];
+let actions = {};
+
+export const useStore = () => {
+  const setState = useState(globalState)[1];
+
+  const dispatch = (actionIdentifier) => {
+    const newState = actions[actionIdentifier](globalState);
+    globalState = { ...globalState, ...newState };
+
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
+
+  useEffect(() => {
+    listeners.push(setState);
+
+    return () => {
+      listeners = listeners.filter((li) => li !== setState);
+    };
+  }, [setState]);
+
+  return [globalState, dispatch];
+};

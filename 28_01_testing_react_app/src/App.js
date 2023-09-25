ce8452a02e08b59dcd13fc5a459f8a92efdb9d92
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-09-26 01:21:03 +0530

    commit :: some changes in Async.js

--- 28_01_testing_react_app/src/App.js
+++ 28_01_testing_react_app/src/App.js
@@ -1,10 +1,12 @@
 import Greeting from "./components/Greeting";
+import Async from "./components/Async";
 import "./App.css";
 // import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
+      <Async />
       <Greeting />
     </div>
   );
---
+++ 28_01_testing_react_app/src/components/Async.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+const Async = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {posts.map((post) => {
+          return <li key={post.id}>{post.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Async;

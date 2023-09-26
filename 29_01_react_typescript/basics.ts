0d23c3810bfdb2555c8c477ac0ff81233b7379b2
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-09-26 20:02:11 +0530

    commit :: typescript code push

---
+++ 29_01_react_typescript/basics.ts
@@ -0,0 +1,14 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// function types, parameters
+
+// Primitives
+
+let age: number;
+age = 12;
+
+let userName: string;
+userName = "Max";
+
+let isInstructor: boolean;
+isInstructor = true;

beb046fd33f2751935c1017b081a81892cfdada1
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-09-26 23:34:17 +0530

    commit :: some changes typescript basis files

--- 29_01_react_typescript/basics.ts
+++ 29_01_react_typescript/basics.ts
@@ -42,5 +42,39 @@ let course = "React - The Complete Guide";
 // course = 1234;
 
 // Union Types
+
 let courses: string | number = "React - The Complete Guide";
 courses = 1234;
+
+// Understanding type aliases
+type Person = {
+  name: string;
+  age: number;
+};
+
+let persona: Person;
+
+// Functions & types
+function add(a: number, b: number) {
+  return a + b;
+}
+
+function printOutPut(value: any) {
+  console.log(value);
+}
+
+// Generics
+// function insertAtBeginning(array: any[], value: any) {
+//   const newArray = [value, ...array];
+//   return newArray;
+// }
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updateArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2,3]
+const stringArray = insertAtBeginning(["a", "b", "c"], "d");

920ec6802a63b0d934734eac02314f9b1354e499
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-09-26 20:22:24 +0530

    commit :: typescript basics code

--- 29_01_react_typescript/basics.ts
+++ 29_01_react_typescript/basics.ts
@@ -12,3 +12,35 @@ userName = "Max";
 
 let isInstructor: boolean;
 isInstructor = true;
+
+// More complex types
+let hobbies: string[];
+hobbies = ["Sports", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+// person = {
+//     isEmployee:true;
+// }
+
+let people: {
+  name: string;
+  age: number;
+}[];
+
+// Type inference
+
+let course = "React - The Complete Guide";
+// course = 1234;
+
+// Union Types
+let courses: string | number = "React - The Complete Guide";
+courses = 1234;

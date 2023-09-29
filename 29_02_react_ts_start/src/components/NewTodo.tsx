154e51e2185595734ae1451ff84b338e8ac50906
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-09-30 00:25:18 +0530

    commit :: form event in tsx

--- 29_02_react_ts_start/src/components/NewTodo.tsx
+++ 29_02_react_ts_start/src/components/NewTodo.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const NewTodo = () => {
-  return <div></div>;
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo text</label>
+      <input type="text" id="text" />
+      <button>Add Todo</button>
+    </form>
+  );
 };
 
 export default NewTodo;

ae71993e2b44b603450291709bfdf707ddfce4ac
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-10-05 00:47:11 +0530

    commit :: some changes in Ingredient form files

--- 30_01_react_hook/src/components/Ingredients/IngredientForm.js
+++ 30_01_react_hook/src/components/Ingredients/IngredientForm.js
@@ -22,7 +22,10 @@ const IngredientForm = React.memo((props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({
+      title: inputState.title,
+      amount: inputState.amount,
+    });
   };
 
   return (
--- 30_01_react_hook/src/components/Ingredients/Ingredients.js
+++ 30_01_react_hook/src/components/Ingredients/Ingredients.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import Search from "./Search";
+import IngredientList from "./IngredientList";
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients((preIngredients) => [
+      ...preIngredients,
+      { id: Math.random().toString(), ...ingredient },
+    ]);
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
         {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;

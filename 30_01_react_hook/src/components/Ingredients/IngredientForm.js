dfb8abf588af3200e6dd599677e40c2e994552eb
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-10-04 23:49:29 +0530

    commit :: some changes in Form file

--- 30_01_react_hook/src/components/Ingredients/IngredientForm.js
+++ 30_01_react_hook/src/components/Ingredients/IngredientForm.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useRef } from "react";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
-const IngredientForm = React.memo(props => {
-  const submitHandler = event => {
+const IngredientForm = React.memo((props) => {
+  const [inputState, setInputState] = useState({
+    title: "",
+    amount: "",
+  });
+
+  const onChangeInputState = (event) => {
+    let declareInputObj = inputState;
+    if (event.target.id === "title") {
+      declareInputObj = { ...inputState, title: event.target.value };
+    }
+    if (event.target.id === "amount") {
+      declareInputObj = { ...inputState, amount: event.target.value };
+    }
+    setInputState(declareInputObj);
+  };
+
+  const submitHandler = (event) => {
     event.preventDefault();
     // ...
   };
@@ -15,11 +31,21 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input
+              type="text"
+              id="title"
+              value={inputState.title}
+              onChange={(e) => onChangeInputState(e)}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input
+              type="number"
+              id="amount"
+              value={inputState.amount}
+              onChange={(e) => onChangeInputState(e)}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>

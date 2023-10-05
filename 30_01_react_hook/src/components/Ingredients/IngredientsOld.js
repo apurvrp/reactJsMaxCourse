90a226e2eda78dcdad8ddcbef16a30fba708bd6e
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-10-05 22:37:04 +0530

    commit :: some changes in Ingredients.js

--- 30_01_react_hook/src/components/Ingredients/IngredientsOld.js
+++ 30_01_react_hook/src/components/Ingredients/IngredientsOld.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
@@ -6,24 +6,6 @@ import IngredientList from "./IngredientList";
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
-
-  useEffect(() => {
-    fetch(
-      "https://testing-apis-46c25-default-rtdb.firebaseio.com/ingredients.json"
-    )
-      .then((response) => response.json())
-      .then((responseData) => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
-          });
-        }
-        setUserIngredients(loadedIngredients);
-      });
-  }, []);
 
   const addIngredientHandler = (ingredient) => {
     fetch(
@@ -33,16 +15,11 @@ const Ingredients = () => {
         body: JSON.stringify(ingredient),
         headers: { "Content-Type": "application/json" },
       }
-    )
-      .then((response) => {
-        return response.json;
-      })
-      .then((responseData) => {
-        setUserIngredients((preIngredients) => [
-          ...preIngredients,
-          { id: responseData.name, ...ingredient },
-        ]);
-      });
+    );
+    setUserIngredients((preIngredients) => [
+      ...preIngredients,
+      { id: Math.random().toString(), ...ingredient },
+    ]);
   };
 
   const deleteIngredientHandler = (ingreId) => {

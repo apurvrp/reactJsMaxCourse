e8f076063d7b585606aff8b72b4268dd16d09e8b
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-10-07 19:02:49 +0530

    commit :: some changes in Ingredient.js file

--- 30_01_react_hook/src/components/Ingredients/IngredientsOld1.js
+++ 30_01_react_hook/src/components/Ingredients/IngredientsOld1.js
@@ -1,52 +1,35 @@
-import React, { useCallback, useEffect, useReducer, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import ErrorModal from "../UI/ErrorModal";
 import Search from "./Search";
 
-const ingredientReducer = (currentIngredients, action) => {
-  switch (action.type) {
-    case "SET":
-      return action.ingredients;
-    case "ADD":
-      return [...currentIngredients, action.ingredient];
-    case "DELETE":
-      return currentIngredients.filter((ing) => ing.id !== action.id);
-    default:
-      throw new Error("Should not get there!");
-  }
-};
-
 const Ingredients = () => {
-  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
-  // const [userIngredients, setUserIsngredients] = useState([]);
+  const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(null);
 
   useEffect(() => {
-    // fetch(
-    //   "https://testing-apis-46c25-default-rtdb.firebaseio.com/ingredients.json"
-    // )
-    //   .then((response) => response.json())
-    //   .then((responseData) => {
-    //     const loadedIngredients = [];
-    //     for (const key in responseData) {
-    //       loadedIngredients.push({
-    //         id: key,
-    //         title: responseData[key].title,
-    //         amount: responseData[key].amount,
-    //       });
-    //     }
-    //     setUserIngredients(loadedIngredients);
-    //   });
-
-    console.log("RENDERING Ingredients", userIngredients);
-  }, [userIngredients]);
+    fetch(
+      "https://testing-apis-46c25-default-rtdb.firebaseio.com/ingredients.json"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
 
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
-    // setUserIngredients(filteredIngredients);
-    dispatch({ type: "SET", ingredients: filteredIngredients });
+    setUserIngredients(filteredIngredients);
   }, []);
 
   const addIngredientHandler = (ingredient) => {
@@ -64,14 +47,10 @@ const Ingredients = () => {
         return response.json;
       })
       .then((responseData) => {
-        // setUserIngredients((preIngredients) => [
-        //   ...preIngredients,
-        //   { id: responseData.name, ...ingredient },
-        // ]);
-        dispatch({
-          type: "ADD",
-          ingredient: { id: responseData.name, ...ingredient },
-        });
+        setUserIngredients((preIngredients) => [
+          ...preIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
       })
       .catch((error) => {
         setIsError(error.message);
@@ -89,10 +68,9 @@ const Ingredients = () => {
     )
       .then((response) => {
         setIsLoading(false);
-        // setUserIngredients((lastIngredients) =>
-        //   lastIngredients.filter((ingredient) => ingredient.id !== ingreId)
-        // );
-        dispatch({ type: "DELETE", id: ingreId });
+        setUserIngredients((lastIngredients) =>
+          lastIngredients.filter((ingredient) => ingredient.id !== ingreId)
+        );
       })
       .catch((error) => {
         setIsError(error.message);

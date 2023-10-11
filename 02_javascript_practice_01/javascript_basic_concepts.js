1c752ca69b3e291356a738a89d38fbd0238ff11d
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-10-11 22:24:39 +0530

    commit :: some changes in index.html

---
+++ 02_javascript_practice_01/javascript_basic_concepts.js
@@ -0,0 +1,106 @@
+import React from "react";
+
+const javascript_basic_concepts = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  /* 
+    API call via fetch functions and axios 
+    ====================================================
+  */
+
+  const getStaffList = async () => {
+    try {
+      const res = await axios({
+        method: "GET",
+        url: `${REACT_APP_API_URL}institute-user/staff/details`,
+        params: {
+          from_date: payload.from_date,
+          to_date: payload.to_date,
+          iid: payload.iid,
+        },
+      });
+      return res.data;
+    } catch (e) {
+      return { error: e };
+    }
+  };
+
+  const submitStaffAttendance = async (payload) => {
+    try {
+      const res = await axios({
+        method: "POST",
+        url: `${REACT_APP_API_URL}staff-attendance/markattendance`,
+        data: payload,
+      });
+      return res.data;
+    } catch (error) {
+      return { error: error };
+    }
+  };
+
+  async function demo(payload) {
+    const res = await axios({
+      method: "POST",
+      url: "dsdas.com",
+      data: payload,
+    });
+  }
+
+  // fetch functions and return promise
+  useEffect(() => {
+    fetch(
+      "https://testing-apis-46c25-default-rtdb.firebaseio.com/ingredients.json"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+
+    // ====================================================================================== //
+
+    fetch("URL", {
+      method: method,
+      body: body,
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        return dispatchHttp({ responseData: responseData });
+      });
+  }, []);
+
+  // Asynchronous Function ::
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://testing-apis-46c25-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+    fetchMoviesHandler();
+  }
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default javascript_basic_concepts;
